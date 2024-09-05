05ae71dd2b9cf2f4f4be87d46ad24d766d4658bd
cabouelw <cabouelw@gmail.com>
2024-09-05 23:11:05 +0100

    feat: install dependencies and config

---
+++ src/lib/routes.ts
@@ -0,0 +1,14 @@
+export const AppRoutes = {
+    home: '/',
+    payment: '/paiement',
+    statistics: '/statistique',
+    partners: '/partenaires',
+    delivery: '/livraisons',
+    associations: '/associations',
+    crm: '/crm',
+    marketing: '/marketing',
+    settings: '/settings',
+    humanResources: '/ressources-humaines',
+    website: '/site-web',
+    parameter: '/parametre',
+}
