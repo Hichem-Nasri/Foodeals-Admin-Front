5bcdddbdea8ba2cd483d77ebb3b764391b1bad35
AymanAkashi <aaggoujj@student.1337.ma>
2024-10-17 17:52:02 +0100

    feat: Add DataTableSkeleton component for displaying skeleton loading state in tables

---
+++ src/components/TableSkeleton.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Skeleton } from '@/components/ui/skeleton'; // Ensure you have a Skeleton component
+
+interface DataTableSkeletonProps {
+    columnCount: number;
+    rowCount: number;
+}
+
+export const DataTableSkeleton: FC<DataTableSkeletonProps> = ({ columnCount, rowCount }) => {
+    return (
+        <div className="w-full overflow-auto rounded-[14px]">
+            <Table className="rounded-[14px] bg-white py-2">
+                <TableHeader>
+                    <TableRow>
+                        {Array.from({ length: columnCount }).map((_, index) => (
+                            <TableHead key={index} className={index === 0 ? 'min-w-48' : 'min-w-40'}>
+                                <Skeleton
+                                    className={`h-6 w-full rounded ${
+                                        index === 0 ? 'lg:w-48' : 'lg:w-40'
+                                    }`}
+                                />
+                            </TableHead>
+                        ))}
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {Array.from({ length: rowCount }).map((_, rowIndex) => (
+                        <TableRow key={rowIndex}>
+                            {Array.from({ length: columnCount }).map((_, cellIndex) => (
+                                <TableCell key={cellIndex} className={cellIndex === 0 ? 'min-w-48' : 'min-w-40'}>
+                                    <Skeleton
+                                        className={`h-4 w-full rounded-full ${
+                                            cellIndex === 0 ? 'lg:w-48' : 'lg:w-40'
+                                        }`}
+                                    />
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
