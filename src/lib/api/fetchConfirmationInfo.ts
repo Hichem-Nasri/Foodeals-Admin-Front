2d17501f3a35085ed45b7b5efd01a426ecccba58
AymanAkashi <aaggoujj@student.1337.ma>
2024-10-31 23:53:31 +0100

    refactor: Update fetchConfirmationInfo to use API function and handle errors

--- src/lib/api/fetchConfirmationInfo.ts
+++ src/lib/api/fetchConfirmationInfo.ts
@@ -1,9 +1,28 @@
-import { ConfirmPaymentType, defaultValuesConfirmPayment } from "@/types/PaymentType"
-import { API_URL } from "."
-import { appApi } from "../routes"
+import {
+    ConfirmPaymentType,
+    defaultValuesConfirmPayment,
+} from '@/types/PaymentType'
+import { API_URL } from '.'
+import { appApi } from '../routes'
+import api from '@/api/Auth'
 
-export const getConfirmationInfo = async (versionId?: string): Promise<ConfirmPaymentType | undefined> => {
-	if (!versionId || !Number.isInteger(Number(versionId))) return undefined
-	const url = API_URL + appApi.partnerDetails.replace("{listingId}", versionId)
-	return defaultValuesConfirmPayment
+export const getConfirmationInfo = async (
+    versionId?: string
+): Promise<ConfirmPaymentType | undefined> => {
+    if (!versionId || !Number.isInteger(Number(versionId))) return undefined
+    try {
+        const response = await api
+            .get(
+                API_URL +
+                    appApi.paymentReceived.replace('{paymentId}', versionId)
+            )
+            .catch((error) => {
+                throw new Error(error)
+            })
+        if (!response || !response.data) return undefined
+        return response.data as ConfirmPaymentType
+    } catch (error) {
+        console.error(error)
+        return undefined
+    }
 }
