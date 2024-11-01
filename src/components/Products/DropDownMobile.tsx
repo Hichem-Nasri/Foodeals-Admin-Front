4c1108537d432b75342a76ef62f0853236ea6cfa
AymanAkashi <aaggoujj@student.1337.ma>
2024-11-01 23:44:34 +0100

    feat: Add DropDownList component for mobile navigation
    - Create a new file, DropDownMobile.tsx, to implement the DropDownList component for mobile navigation
    - Import necessary components and icons from existing files
    - Define the DropDownListProps interface to specify the required props for the component
    - Implement the DropDownList component as a functional component
    - Add the necessary JSX structure and styling for the dropdown menu
    - Handle click events for the dropdown menu items to trigger the corresponding actions
    - Export the DropDownList component as the default export of the file

---
+++ src/components/Products/DropDownMobile.tsx
@@ -0,0 +1,86 @@
+import React, { FC } from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import {
+    FileSpreadsheet,
+    ImagePlus,
+    ListTodo,
+    Plus,
+    QrCode,
+} from 'lucide-react'
+import { CustomButton } from '../custom/CustomButton'
+import Link from 'next/link'
+import { AppRoutes } from '@/lib/routes'
+import { NewImageProduct } from './newProducts/newImage'
+import { useState } from 'react'
+// import AppRouter from 'next/dist/client/components/app-router';
+
+interface DropDownListProps {
+    list: {
+        label: string
+        href: string
+        icon: any
+    }[]
+    children: React.ReactNode
+    setSheet: React.Dispatch<React.SetStateAction<boolean>>
+    setImage: React.Dispatch<React.SetStateAction<boolean>>
+    isMobile?: boolean
+}
+
+const DropDownList: FC<DropDownListProps> = ({
+    list,
+    children,
+    setSheet,
+    setImage,
+    isMobile = false,
+}) => {
+    const style =
+        'flex items-center gap-[10px] justify-between w-full text-white bg-primary rounded-[18px] p-[20px]'
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="focus:outline-none">
+                    {children}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="rounded-[16px] bg-transparent border-0 shadow-none drop-shadow-none p-0 px-0 py-0 gap-0">
+                    <div className="flex flex-col gap-0 px-2 bg-transparent w-full">
+                        <DropdownMenuItem>
+                            <Link href={AppRoutes.newProduct} className={style}>
+                                <span>AJOUTER UN PRODUIT</span>
+                                <Plus size={22} />
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    setImage((prev) => !prev)
+                                }}
+                                className={style}
+                            >
+                                <span>SCANNER UN PRODUIT</span>
+                                <QrCode size={22} />
+                            </button>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <button
+                                type="button"
+                                onClick={() => setSheet((prev) => !prev)}
+                                className={style}
+                            >
+                                <span>IMPORTER DES PRODUITS</span>
+                                <FileSpreadsheet size={22} />
+                            </button>
+                        </DropdownMenuItem>
+                    </div>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
+
+export default DropDownList
---
+++ src/components/Products/ProductCard.tsx
@@ -0,0 +1,67 @@
+import React, { FC } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { ProductType } from '@/types/products'
+import { Calendar, Eye, PencilLine } from 'lucide-react'
+import { AppRoutes } from '@/lib/routes'
+import Link from 'next/link'
+
+interface ProductCardProps {
+    product: ProductType
+}
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
+    return (
+        <div className="flex flex-col gap-3 bg-white p-3 h-28 rounded-[20px] min-w-[400px]">
+            <div className="flex justify-between items-center h-full w-full">
+                <div className="flex flex-col justify-center gap-[3px] w-full">
+                    <div className="justify-self-start flex justify-start items-start space-x-1 ">
+                        <Avatar className="w-[46px] h-[46px]">
+                            <AvatarImage
+                                src={product.product.avatar}
+                                sizes="46px"
+                            />
+                            <AvatarFallback>
+                                {product.product.name &&
+                                    product.product.name[0]}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="gap-4">
+                            <h1 className="text-lg">{product.product.name}</h1>
+                            <h3 className="text-sm text-mountain-400">
+                                {product.category}
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="justify-center text-lynch-500 flex justify-items-center space-x-1.5">
+                        <Calendar size={18} />
+                        <h3 className="text-sm ">{product.date}</h3>
+                    </div>
+                </div>
+                <div className="gap-3 w-full flex justify-end items-center">
+                    <Link
+                        href={
+                            AppRoutes.ProductDetails.replace(
+                                ':id',
+                                product.id
+                            ) + '?mode=edit'
+                        }
+                        className="size-11 rounded-full text-white bg-lynch-300 flex justify-center items-center hover:bg-lynch-500 transition-colors"
+                    >
+                        <PencilLine size={24} />
+                    </Link>
+                    <Link
+                        href={AppRoutes.ProductDetails.replace(
+                            ':id',
+                            product.id
+                        )}
+                        className="size-11 rounded-full text-white bg-lynch-300 flex justify-center items-center hover:bg-lynch-500 transition-colors"
+                    >
+                        <Eye size={24} />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
