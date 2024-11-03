f13b1a4826023545efaf2bc8526d0ca8a8baf81d
AymanAkashi <aaggoujj@student.1337.ma>
2024-11-03 13:05:24 +0100

    feat: Add react-circular-progressbar for progress circle in ProgressCircle.tsx

--- src/components/Products/newProducts/ProgressCircle.tsx
+++ src/components/Products/newProducts/ProgressCircle.tsx
@@ -1,39 +1,73 @@
 import { useEffect, useState } from 'react'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 interface Props {
     totalFiles: number
     completedFiles: number
+    withText?: boolean
 }
 
-const ProgressCircle = ({ totalFiles, completedFiles }: Props) => {
+const ProgressCircle = ({
+    totalFiles,
+    completedFiles,
+    withText = true,
+}: Props) => {
     const [percentage, setPercentage] = useState(0)
 
     // Calculate the percentage based on the completed files
     useEffect(() => {
+        if (totalFiles === 0 || totalFiles <= completedFiles) return
         setPercentage(Math.round((completedFiles / totalFiles) * 100))
     }, [completedFiles, totalFiles])
 
     return (
         <div className="flex flex-col items-center">
-            <div className="relative w-24 h-24 rounded-full">
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16 rounded-full bg-gray-200" />
-                <div
-                    className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16 rounded-full bg-green-500 transition-all duration-500 ease-in-out ${
-                        percentage === 100 && 'animate-spin'
-                    }`}
-                    style={{ transform: `rotate(${percentage * 3.6}deg)` }}
+            <div className="relative w-48 h-48 rounded-full">
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[calc(50%-20px)] text-lynch-400 text-lg">
+                    {totalFiles} fichier
+                </div>
+                <CircularProgressbar
+                    value={percentage}
+                    text={`${percentage}%`}
+                    styles={{
+                        root: {},
+                        path: {
+                            stroke: `rgba(52,211,158)`,
+                            transition: 'stroke-dashoffset 0.5s ease 0s',
+                            transformOrigin: 'center center',
+                            strokeWidth: 4,
+                        },
+                        trail: {
+                            stroke: 'rgb(236 ,238 ,242)',
+                            strokeLinecap: 'butt',
+                            transform: 'rotate(0.25turn)',
+                            transformOrigin: 'center center',
+                            strokeWidth: 4,
+                        },
+                        text: {
+                            fill: '#23272e',
+                            fontSize: '16px',
+                            fontWeight: 'semi-bold',
+                            padding: '0px',
+                            translate: '0 -4px',
+                        },
+                        background: {
+                            fill: '#34d39e',
+                        },
+                    }}
                 />
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-900 font-bold text-xl">
-                    {percentage}%
-                </div>
             </div>
-            <div className="mt-2 text-gray-600 text-sm">
-                {completedFiles} fichiers chargés sur {totalFiles}
-            </div>
-            {percentage === 100 && (
-                <div className="mt-2 text-green-500 font-medium">
-                    Fichiers chargés avec succès
-                </div>
+            {completedFiles === totalFiles && withText ? (
+                <h1 className="text-sm text-mountain-500">
+                    Fichier chargé avec succès
+                </h1>
+            ) : (
+                withText && (
+                    <h1 className="text-sm">
+                        Fichier en cours de chargement...
+                    </h1>
+                )
             )}
         </div>
     )
